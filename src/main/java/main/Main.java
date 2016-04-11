ef2980ad7e536614450a6a42a902205ce9664f92
CarnivoreBarnacle <antti.saikkonen@hotmail.fi>
2016-04-11 15:34:44 +0300

    herokufixmaybe???

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,10 +7,10 @@ import org.springframework.context.ApplicationContext;
 
 
 @SpringBootApplication
-public class App {
+public class Main {
     
     
     public static void main(String[] args){
-        ApplicationContext ctx = SpringApplication.run(App.class, args);
+        ApplicationContext ctx = SpringApplication.run(Main.class, args);
     }
 }
