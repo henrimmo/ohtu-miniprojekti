bcfcea2698fed41a6c841c17324a10cd6263dc30
CarnivoreBarnacle <antti.saikkonen@hotmail.fi>
2016-04-11 14:57:44 +0300

    Fixing heroku...

--- src/main/java/main/TestController.java
+++ src/main/java/main/TestController.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@ResponseBody
 public class TestController {
     
     @RequestMapping("/")
+    @ResponseBody
     public String test(){
         return "Pallot - ohtu-miniprojekti";
     }
