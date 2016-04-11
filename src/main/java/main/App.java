026a1785e031872dc3395dd40376a7f8538ddd17
CarnivoreBarnacle <antti.saikkonen@hotmail.fi>
2016-04-11 15:38:45 +0300

    ...

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -7,10 +7,10 @@ import org.springframework.context.ApplicationContext;
 
 
 @SpringBootApplication
-public class Main {
+public class App {
     
     
     public static void main(String[] args){
-        ApplicationContext ctx = SpringApplication.run(Main.class, args);
+        ApplicationContext ctx = SpringApplication.run(App.class, args);
     }
 }
