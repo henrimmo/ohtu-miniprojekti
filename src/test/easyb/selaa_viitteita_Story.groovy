99b44d8a49f6df800e335bff84f83564d26ba896
Outi Jussila <outi.jussila@helsinki.fi>
2016-04-11 15:38:55 +0300

    testit omissa tiedostoissa

---
+++ src/test/easyb/selaa_viitteita_Story.groovy
@@ -0,0 +1,28 @@
+description 'Käyttäjä pystyy selaamaan talletettuja viitteitä'
+
+scenario "talletetut viitteet näytetään listana listaus-välilehdellä", {
+	given 'käyttäjä on etusivulla', {
+	}
+	when 'listaus-välilehti valitaan', {
+	}
+	then 'tietokannan sisältö näkyy listana', {
+	}
+}
+
+scenario "talletetut viitteet näytetään lisäysjäsjestyksessä", {
+	given 'käyttäjä on etusivulla', {
+	}
+	when 'listaus-välilehti valitaan', {
+	}
+	then 'tietokannan sisältö näkyy lisäysjärjestyksessä', {
+	}
+}
+
+scenario "viitteen tiedot on pilkuilla erotettuja listaus-välilehdellä", {
+	given 'käyttäjä on etusivulla', {
+	}
+	when 'listaus-välilehti valitaan', {
+	}
+	then 'listassa jokaisen viitteen tiedot on pilkuilla erotettuja', {
+	}
+}
