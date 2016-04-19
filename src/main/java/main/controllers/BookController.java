796fdacf317f38531ebd84e7c9c1dd7fee622b19
Outi Jussila <outi.jussila@helsinki.fi>
2016-04-19 10:57:29 +0300

    kontrollereita book ja inproceedings

---
+++ src/main/java/main/controllers/BookController.java
@@ -0,0 +1,47 @@
+
+package main.controllers;
+
+import main.models.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import main.repositories.BookRepository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class BookController {
+    @Autowired
+    private BookRepository bookRepository;
+    
+    //Simple test for database
+    @RequestMapping("/test")
+    @ResponseBody
+    public String bookRepositoryTest(){
+        if(bookRepository.findAll().isEmpty()){
+            Book b = new Book();
+            b.setTitle("Test name");
+            b.setPublisher("Test journal");
+            b.setAuthor("Test Author");
+            b.setYear(2016);
+            bookRepository.save(b);
+        }
+        
+        Book r = bookRepository.findOne(1L);
+        return r.toBibTex();
+    }
+    
+    @RequestMapping(value="/new", method=RequestMethod.GET)
+    public String greetingForm(Model model) {
+        model.addAttribute("article", new Book());
+        return "book";
+    }
+
+    @RequestMapping(value="/new", method=RequestMethod.POST)
+    public String greetingSubmit(@ModelAttribute Book book, Model model) {
+        bookRepository.save(book);
+        model.addAttribute("book", book);
+        return "result";
+    }
+}
