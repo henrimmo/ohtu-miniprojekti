eca0339fbd1ee2e8310bc8ad75317a7ad8a33c3a
Tuomas Siipola <siiptuo@kapsi.fi>
2016-04-23 20:02:54 +0300

    BibTexGenerator-testi

---
+++ src/test/java/main/BibTexGeneratorTest.java
@@ -0,0 +1,24 @@
+package main;
+
+import main.models.Article;
+import main.models.Book;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BibTexGeneratorTest {
+
+    @Test
+    public void testGenerate() {
+        Article article = new main.models.Article("puu08", "Mr. Puu", "Kuuset", "Luonto", 2008);
+        Book book = new Book("liisa73", "Liisa", "Viikossa", "Kuustonen", 1873);
+
+        BibTexGenerator generator = new BibTexGenerator();
+        generator.addEntry(book);
+        generator.addEntry(article);
+
+        String bibtex = generator.generate();
+        assertTrue(bibtex.contains(article.toBibTex()));
+        assertTrue(bibtex.contains(book.toBibTex()));
+    }
+
+}
