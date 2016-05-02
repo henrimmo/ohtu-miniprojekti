1e76babc75b9e17c69b5d0a92e0e16d905bfc474
Antti Saikkonen <antti.saikkonen@hotmail.fi>
2016-05-03 00:06:07 +0300

    Update ManualTest.java

--- src/test/java/main/models/ManualTest.java
+++ src/test/java/main/models/ManualTest.java
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-//
-//package main.models;
-//
-//import main.services.BibTexGenerator;
-//import org.junit.Test;
-//import static org.junit.Assert.*;
-//
-//public class ManualTest {
-//    
-//    private final String title1 = "Kissakirja2";
-//    private final Manual manual = new Manual("kk", title1);
-//    
-//    public ManualTest() {
-//    }
-//    
-//    /**
-//     * Test of setTitle method, of class Manual.
-//     */
-//    @Test
-//    public void testSetTitle() {
-//        String title = "Oravat kuussa";
-//        Manual instance = new Manual();
-//        instance.setTitle(title);
-//        assertEquals(instance.getTitle(), "Oravat kuussa");
-//    }
-//
-//    /**
-//     * Test of getTitle method, of class Manual.
-//     */
-//    @Test
-//    public void testGetTitle() {
-//        String expResult = "Kissakirja2";
-//        String result = manual.getTitle();
-//        assertEquals(expResult, result);
-//    }
-//
-//    
-//    @Test
-//    public void testToBibText() {
-//        String bibTex = BibTexGenerator.generateEntry(manual);
-//        assertTrue(bibTex.contains("@booklet{kk"));
-//        assertTrue(bibTex.contains("title = {Kissakirja2}"));
-//    }
-//    
-//}
-=======
-
 package main.models;
 
 import main.services.BibTexGenerator;
@@ -90,4 +42,3 @@ public class ManualTest {
     }
     
 }
->>>>>>> 6f116a91d936d1bc142149725076ba4a37f7a490
