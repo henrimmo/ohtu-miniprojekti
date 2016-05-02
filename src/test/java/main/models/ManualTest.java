7bfcf84c498d5c21c7e8a262339b1c6a4b3fc6c6
Outi Jussila <outi.jussila@helsinki.fi>
2016-05-02 23:21:36 +0300

    manual-formaatti2

---
+++ src/test/java/main/models/ManualTest.java
@@ -0,0 +1,45 @@
+
+package main.models;
+
+import main.services.BibTexGenerator;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ManualTest {
+    
+    private final String title1 = "Kissakirja2";
+    private final Manual manual = new Manual("kk", title1);
+    
+    public ManualTest() {
+    }
+    
+    /**
+     * Test of setTitle method, of class Manual.
+     */
+    @Test
+    public void testSetTitle() {
+        String title = "Oravat kuussa";
+        Manual instance = new Manual();
+        instance.setTitle(title);
+        assertEquals(instance.getTitle(), "Oravat kuussa");
+    }
+
+    /**
+     * Test of getTitle method, of class Manual.
+     */
+    @Test
+    public void testGetTitle() {
+        String expResult = "Kissakirja2";
+        String result = manual.getTitle();
+        assertEquals(expResult, result);
+    }
+
+    
+    @Test
+    public void testToBibText() {
+        String bibTex = BibTexGenerator.generateEntry(manual);
+        assertTrue(bibTex.contains("@booklet{kk"));
+        assertTrue(bibTex.contains("title = {Kissakirja2}"));
+    }
+    
+}
